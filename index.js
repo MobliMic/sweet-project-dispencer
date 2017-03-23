ae2abdd681e42503dab5bb3d05a9a17991ed3540
Michael Craddock <michael@moblimic.com>
2017-03-23 15:24:18 +0000

    working on controlling the servo

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
 var Gpio = require('pigpio').Gpio,
-  motor = new Gpio(10, {mode: Gpio.OUTPUT}),
-  pulseWidth = 1000,
-  increment = 10;
+motor = new Gpio(10, {mode: Gpio.OUTPUT});
+var url = require('url');
+
+// servo disk release point
+var centerPoint = 1000;
+
+// servo state
+var currentLocation = 0;
+
+// on run reset the servo
+motor.servoWrite(currentLocation);
+
+
 
 var button = new Gpio(4, {
-    mode: Gpio.INPUT,
-    pullUpDown: Gpio.PUD_DOWN,
-    edge: Gpio.EITHER_EDGE
-  });
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.EITHER_EDGE
+});
 
 button.on('interrupt', function (level) {
-  motor.servoWrite(pulseWidth);
-  pulseWidth += increment;
-	  if (pulseWidth >= 2000) {
-	    increment = -10;
-	  } else if (pulseWidth <= 1000) {
-	    increment = 10;
-  	}
+  motor.servoWrite(centerPoint);
 });
 
 var http = require('http');
 var port = 80;
 
 var reqHand = function(req, res) {
-setInterval(function () {
-  motor.servoWrite(pulseWidth);
+  var queryData = url.parse(request.url, true).query;
+  console.log(queryData);
 
-  pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -10;
-  } else if (pulseWidth <= 1000) {
-    increment = 10;
+  if(currentLocation <= centerPoint){
+    currentLocation = centerPoint + 800;
+  } else {
+    currentLocation = centerPoint - 800;
   }
-}, 10);
 
-	res.end('acknowledged commander!');
+  if(currentLocation < 0){
+    currentLocation = 0;
+  } else if (currentLocation > 2000) {
+    currentLocation = 2000;
+  }
+
+  if(queryData.pos){
+    currentLocation = queryData.pos;
+    console.log(queryData.pos);
+  }
+
+  motor.servoWrite(currentLocation);
+
+  res.end('acknowledged commander!');
 }
 
 var server = http.createServer(reqHand);
 
 server.listen(port, function(err){
-	if(err){
-		return console.log('uh oh! : ', err);
-	}
+  if(err){
+    return console.log('uh oh! : ', err);
+  }
 
-	console.log('go chicken go');
+  console.log('go chicken go');
 });
